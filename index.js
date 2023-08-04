ba74c0f87cbf6a323132a614d7ea04d1b0ee26f2
Federico <fede4488@gmail.com>
2023-08-04 18:55:09 -0300

    second edit

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ import { menuArray } from "data.js"
 
 
 const renderMenuEl = document.getElementById("feed")
+
+function displayMenu(){
+    let renderMenu = ''
+    menuArray.forEach(function(menuItem){
+        renderMenu = ``
+    
+    })
+}
